4bd0987776363ac9da698e10923ad122645c8238
o0Ignition0o <jeremy.lempereur@gmail.com>
2018-12-11 17:44:12 +0100

    Adding a tokio runtime + some refactoring.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate futures;
 extern crate nitox;
+extern crate tokio;
 use futures::{future::ok, prelude::*};
 use nitox::{commands::*, NatsClient, NatsClientOptions, NatsError};
 
@@ -16,23 +17,45 @@ fn connect_to_nats() -> impl Future<Item = NatsClient, Error = NatsError> {
         .and_then(|client| ok(client))
 }
 
-fn main() {
-    let _ = connect_to_nats()
-        .and_then(|client| {
-            client.subscribe(SubCommand::builder().subject("Race").build().unwrap());
-            ok(client)
-        })
-        .and_then(|client| {
-            client.publish(
-                PubCommand::builder()
-                    .subject("Race")
-                    .payload("1")
-                    .build()
-                    .unwrap(),
-            )
-        })
-        .and_then(|()| {
+fn unsubscribe_from_race(client: NatsClient) -> impl Future<Item = NatsClient, Error = NatsError> {
+    client
+        .unsubscribe(UnsubCommand::from(
+            SubCommand::builder().subject("Race").build().unwrap(),
+        ))
+        .and_then(|_| ok(client))
+}
+
+fn subscribe_to_race(client: NatsClient) -> impl Future<Item = NatsClient, Error = NatsError> {
+    client
+        .subscribe(SubCommand::builder().subject("Race").build().unwrap())
+        .and_then(|_| ok(client))
+}
+
+fn publish_to_race(client: NatsClient) -> impl Future<Item = NatsClient, Error = NatsError> {
+    client
+        .publish(
+            PubCommand::builder()
+                .subject("Race")
+                .payload("Hello world !")
+                .build()
+                .unwrap(),
+        )
+        .and_then(|_| ok(client))
+}
+
+fn get_tasks_to_perform() -> impl Future<Item = (), Error = ()> {
+    connect_to_nats()
+        .and_then(subscribe_to_race)
+        .and_then(publish_to_race)
+        .and_then(unsubscribe_from_race)
+        .and_then(|_| {
             println!("Done !");
             ok(())
-        });
+        })
+        .map_err(|_| println!("Something terrible happened !"))
+}
+
+fn main() {
+    let tasks = get_tasks_to_perform();
+    tokio::run(tasks);
 }
