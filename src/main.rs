7425a20230cf92f8f93e4b20bb4bd182c7c36c28
o0Ignition0o <jeremy.lempereur@gmail.com>
2018-12-12 13:46:01 +0100

    Incrementing internal counter.

--- src/main.rs
+++ src/main.rs
@@ -28,15 +28,32 @@ fn unsubscribe_from_race(client: NatsClient) -> impl Future<Item = NatsClient, E
 fn handle_message_stream(
     message_stream: impl Stream<Item = Message, Error = NatsError> + Send + 'static,
 ) -> impl Future<Item = (), Error = NatsError> {
-    tokio::spawn(
+    tokio::spawn({
+        let mut internal_count: u32 = 0;
+        println!("Ready to count !");
         message_stream
-            .for_each(move |msg| {
-                println!("Received a message ! {:?}", msg);
-                ok(())
+            .for_each(move |msg| match std::str::from_utf8(&msg.payload) {
+                Ok(payload) => match payload.parse::<u32>() {
+                    Ok(u32_payload) => {
+                        internal_count += u32_payload;
+                        println!("Internal count is now : {}", internal_count);
+                        ok(())
+                    }
+                    Err(_) => {
+                        println!("{} is not a valid unsigned integer ! ", payload);
+                        ok(())
+                    }
+                },
+                Err(_) => {
+                    println!(
+                        "{:?} could not convert payload to an utf8 str !",
+                        msg.payload
+                    );
+                    ok(())
+                }
             })
-            .into_future()
-            .map_err(|_| ()),
-    );
+            .map_err(|_| ())
+    });
     ok(())
 }
 
@@ -47,12 +64,15 @@ fn subscribe_to_race(client: NatsClient) -> impl Future<Item = NatsClient, Error
         .and_then(|_| ok(client))
 }
 
-fn publish_to_race(client: NatsClient) -> impl Future<Item = NatsClient, Error = NatsError> {
+fn publish_to_race(
+    client: NatsClient,
+    payload: String,
+) -> impl Future<Item = NatsClient, Error = NatsError> {
     client
         .publish(
             PubCommand::builder()
                 .subject("Race")
-                .payload("Hello world !")
+                .payload(payload)
                 .build()
                 .unwrap(),
         )
@@ -62,12 +82,10 @@ fn publish_to_race(client: NatsClient) -> impl Future<Item = NatsClient, Error =
 fn get_tasks_to_perform() -> impl Future<Item = (), Error = ()> {
     connect_to_nats()
         .and_then(subscribe_to_race)
-        .and_then(publish_to_race)
+        .and_then(|client| publish_to_race(client, "42".into()))
+        .and_then(|client| publish_to_race(client, "64".into()))
         .and_then(unsubscribe_from_race)
-        .and_then(|_| {
-            println!("Done !");
-            ok(())
-        })
+        .and_then(|_| ok(()))
         .map_err(|_| println!("Something terrible happened !"))
 }
 
