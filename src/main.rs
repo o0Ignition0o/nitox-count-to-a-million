2d54e8390890f312aa1ea25b52e4b69b40b8b5e5
o0Ignition0o <jeremy.lempereur@gmail.com>
2018-12-12 17:24:25 +0100

    Repsonse in a handler.

--- src/main.rs
+++ src/main.rs
@@ -57,10 +57,66 @@ fn handle_message_stream(
     ok(())
 }
 
+fn handle_second_message_stream(
+    message_stream: impl Stream<Item = Message, Error = NatsError> + Send + 'static,
+) -> impl Future<Item = (), Error = NatsError> {
+    tokio::spawn({
+        connect_to_nats().map_err(|_| ()).and_then(|client| {
+            message_stream
+                .for_each(move |msg| match std::str::from_utf8(&msg.payload) {
+                    Ok(payload) => {
+                        println!("Received {} on the second race channel", payload);
+                        // Publish to an other channel
+                        let response_command = PubCommand::builder()
+                            .subject("Race")
+                            .payload("123")
+                            .build()
+                            .unwrap();
+                        let _ = client.publish(response_command);
+                        ok(())
+                    }
+                    Err(_) => {
+                        println!(
+                            "{:?} could not convert payload to an utf8 str !",
+                            msg.payload
+                        );
+                        ok(())
+                    }
+                })
+                .map_err(|_| ())
+        })
+    });
+    ok(())
+}
+
 fn subscribe_to_race(client: NatsClient) -> impl Future<Item = NatsClient, Error = NatsError> {
     client
         .subscribe(SubCommand::builder().subject("Race").build().unwrap())
         .and_then(move |message_stream| handle_message_stream(message_stream))
+        .and_then(|_| ok(client))
+}
+
+fn subscribe_to_second_race(
+    client: NatsClient,
+) -> impl Future<Item = NatsClient, Error = NatsError> {
+    client
+        .subscribe(SubCommand::builder().subject("Race2").build().unwrap())
+        .and_then(move |message_stream| handle_second_message_stream(message_stream))
+        .and_then(|_| ok(client))
+}
+
+fn publish_to_second_race(
+    client: NatsClient,
+    payload: String,
+) -> impl Future<Item = NatsClient, Error = NatsError> {
+    client
+        .publish(
+            PubCommand::builder()
+                .subject("Race2")
+                .payload(payload)
+                .build()
+                .unwrap(),
+        )
         .and_then(|_| ok(client))
 }
 
@@ -82,7 +138,9 @@ fn publish_to_race(
 fn get_tasks_to_perform() -> impl Future<Item = (), Error = ()> {
     connect_to_nats()
         .and_then(subscribe_to_race)
+        .and_then(subscribe_to_second_race)
         .and_then(|client| publish_to_race(client, "42".into()))
+        .and_then(|client| publish_to_second_race(client, "HELLO".into()))
         .and_then(|client| publish_to_race(client, "64".into()))
         .and_then(unsubscribe_from_race)
         .and_then(|_| ok(()))
